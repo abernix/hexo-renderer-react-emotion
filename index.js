f85bfd5f057b9c0a92d95593996f088162d1ccf4
Jesse Rosenberger <git@jro.cc>
2018-08-23 14:03:23 +0300

    Switch to using `babel-register`, rather than `babel-core`'s `transform`.
    This helps resolve the problem where downstream `require`s in the file we're
    rendering were (a) not available, though this could have been fixed through
    a custom `require` implementation, (b) more importantly, lack of
    transpilation of these `require`ed files, which is necessary since they're
    likely to be JSX and utilizing newer ECMAScript synax (namely,
    `import`/`export` syntax!).

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 "use strict"
 
 const vm = require("vm");
-const babel = require("@babel/core");
+const { dirname } = require("path");
 const assert = require("assert");
 
 const { createElement } = require("react");
@@ -12,6 +12,14 @@ const { renderStylesToString } = require("emotion-server");
 assert(typeof hexo === "object",
   "Hexo is required to use `hexo-renderer-react-emotion`.");
 
+const babelOptions = {
+  presets: [
+    require.resolve("@babel/preset-env"),
+    require.resolve("@babel/preset-react"),
+  ],
+  extensions: [".jsx", ".js"],
+};
+
 function renderer (data, options) {
   if (!data.text) {
     console.error("Not enough stuff to render", data.path);
@@ -19,20 +27,29 @@ function renderer (data, options) {
 
   const sandbox = {
     require,
+    ____babelOptions: {
+      ...babelOptions,
+      cwd: dirname(data.path),
+    },
+    console: {
+      log(...args) { console.log(...args) },
+      error(...args) { console.error(...args) },
+    },
     exports: Object.create(null),
   };
 
-  const babeled = babel.transform(data.text, {
-    presets: [
-      require.resolve("@babel/preset-env"),
-      require.resolve("@babel/preset-react"),
-    ],
-  });
+  const code = `
+    require("@babel/register")(____babelOptions);
+    exports = require(${JSON.stringify(data.path)});
+  `;
 
-  vm.runInNewContext(babeled.code, sandbox, {
-    displayErrors: true,
-    filename: data.path,
-  });
+  try {
+    vm.runInNewContext(code, sandbox, {
+      displayErrors: true,
+    });
+  } catch (err) {
+    console.error("ERROR", err);
+  }
 
   assert(sandbox.exports && typeof sandbox.exports.default === "function",
     `Must export a 'default' export from ${data.path}`);
